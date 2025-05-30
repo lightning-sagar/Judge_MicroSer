1790be7c38ce725c2ff145288367caa7d3520580
Sagar Seth <lightningsagar0@gmail.com>
2025-05-30 22:27:34 +0530

    OPEN FOR ALL

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from "cors"
 import submission from './routes/submission.route.js'
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({ extended: true }));
 
 
@@ -13,7 +15,7 @@ app.get('/', (req, res) => {
 
 
 app.use('/api/c',submission);
-
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log('Main backend server running on port 3000');
 });
